f12c6eb024a74a418f75fafa1c26da7ff32c8131
Lawrence <pokepro95@gmail.com>
2022-07-05 00:25:41 -0700

    Create Rock Paper Scissors game code

--- rps.js
+++ rps.js
@@ -2,19 +2,48 @@ function computerPlay(){
     let compChoice = Math.floor(Math.random() * 3);
     switch (compChoice){
         case 0:
-            return "Rock";
+            return "rock";
             break;
         case 1: 
-            return "Paper";
+            return "paper";
             break;
         case 2: 
-            return "Scissors";
+            return "scissors";
             break;
     }
 }
 
 function playRPS(playSelection, computerSelection){
-    
+    let player = playSelection.toLowerCase();
+    switch (player){
+        case ("rock"):
+            if (computerSelection === "scissors"){
+                return "You win! Rock beats Scissors";
+            } else if (computerSelection === "paper"){
+                return "You Lose! Paper beats Rock";
+            } else {
+                return "Tie! Rock does nothing to Rock"
+            }
+            break;
+        case ("paper"):
+            if (computerSelection === "rock"){
+                return "You Win! Paper beats Rock";
+            } else if (computerSelection === "scissors"){
+                return "You Lose! Scissors beats Paper";
+            } else {
+                return "Tie! Paper does nothing to Paper"
+            }
+            break;
+        case("scissors"):
+            if (computerSelection === "paper"){
+                return "You Win! Scissors beats Paper";
+            } else if (computerSelection === "rock"){
+                return "You Lose! Rock beats Scissors";
+            } else {
+                return "Tie! Scissors does nothing to Scissors"
+            }
+            break;
+    }
 }
 
-console.log(computerPlay());
+console.log(playRPS("Rock", computerPlay()));
